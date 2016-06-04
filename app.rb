2d30dbd430f92240118d63663b6ecdc98ba49dc4
kakakakakku <y.yoshida22@gmail.com>
2016-06-05 02:12:36 +0900

    Added blink1

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 require 'sinatra/namespace'
+require 'sinatra/reloader'
+require 'blink1'
 
 set :bind, '0.0.0.0'
 
@@ -8,8 +10,32 @@ namespace '/keynote' do
     system("osascript -e 'tell application \"Keynote\" to activate'")
     system("osascript -e 'tell application \"System Events\" to key code 123'")
   end
+
   get '/next' do
     system("osascript -e 'tell application \"Keynote\" to activate'")
     system("osascript -e 'tell application \"System Events\" to key code 124'")
+  end
+end
+
+namespace '/blink1' do
+  get '/red' do
+    Blink1.open do |blink1|
+     blink1.delay_millis = 2000
+     blink1.blink(255, 0, 0, 1)
+    end
+  end
+
+  get '/green' do
+    Blink1.open do |blink1|
+     blink1.delay_millis = 2000
+     blink1.blink(0, 255, 0, 1)
+    end
+  end
+
+  get '/blue' do
+    Blink1.open do |blink1|
+     blink1.delay_millis = 2000
+     blink1.blink(0, 0, 255, 1)
+    end
   end
 end
